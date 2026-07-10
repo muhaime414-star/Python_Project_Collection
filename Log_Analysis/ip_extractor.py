621e12598d682b2aa3875a4934a39ea630f79f15
muhaime414-star <muhaime414@gmail.com>
2026-07-10 19:52:58 +0800

    added_the_program

---
+++ Log_Analysis/ip_extractor.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+parser = argparse.ArgumentParser(description="Extreact unique IPs from a log file.")
+parser.add_argument('-f', '--file', required=True, help= "The input log file to process")
+parser.add_argument('-o', '--output', help="The name of the custom output file (optional)")
+args = parser.parse_args()
+input_file=args.file 
+
+
+
+if args.output:
+    output_file=args.output
+else:
+    base_name=os.path.basename(input_file)
+    output_file=f"duplicate_free_{base_name}"
+
+unique_ip=[]
+
+try:
+    with open(input_file,'r') as file:
+        logs=file.readlines()
+    for line in logs:
+        clean_ip = line.strip()
+
+        if clean_ip not in unique_ip:
+            unique_ip.append(clean_ip)
+
+    with open(output_file,'w')as out_file:
+        for ip in unique_ip:
+            out_file.write(f"{ip}\n")
+
+    print(f"Success! {len(unique_ip)} unique IPs from {len(logs)} addresses have been saved to '{output_file}'")
+
+
+except FileNotFoundError:
+    print(f"Error: The file '{input_file}' was not found. Please check the name and try again.")
